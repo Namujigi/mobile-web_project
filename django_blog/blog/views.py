f4ec122de296f2de56a4f4ee0968cd0b2b063be6
namujigi <gaea123456@khu.ac.kr>
2025-10-08 05:17:46 +0900

    add image function

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # me = User.objects.get(username='admin')
+    # posts = Post.objects.filter(author=me).order_by('-published_date')
+    posts = Post.objects.order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
